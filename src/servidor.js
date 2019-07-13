69199c6f64488310a2aea979665cc54d8ff706b2
Igor Cerqueira <igor_vc@hotmail.com>
2019-07-13 16:13:51 -0300

    requisicoes e respostas api

--- src/servidor.js
+++ src/servidor.js
@@ -2,13 +2,39 @@ const porta = 3003
 
 const express = require('express')
 const app = express()
+const bancoDeDados = require('./bancoDados')
+const bodyParser = require('body-parser')
 
-app.get('/produtos', (req,res,next) => {
-    console.log('Midleware 1...')
-    next()
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/produtos', (req, res, next) => {
+    res.send(bancoDeDados.getProduts())
 })
-app.get('/produtos', (req,res,next) => { /*padrão middleware*/
-    res.send({nome: 'notebook', preco: 1235.90}) //converte para json automaticamente - metodo send
+
+app.get('/produtos/:id', (req, res, next) => {
+    res.send(bancoDeDados.getProduto(req.params.id))
+})
+
+app.post('/produtos', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+    res.send(produto)
+})
+
+app.put('/produtos/:id', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        id: req.body.id,
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+    res.send(produto)
+})
+
+app.delete('/produtos/:id', (req, res, next) => {
+    const produto = bancoDeDados.excluirProduto(req.params.id)
+    res.send(produto)
 })
 
 app.listen(porta, () => {
